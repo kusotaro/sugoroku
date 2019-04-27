cad95a97f8df43f6b9aed0c4cbdda3c1439062e7
kusotaro <kusotaro@gmail.com>
2019-04-27 22:46:28 +0900

    iroiro

--- sugoroku.py
+++ sugoroku.py
@@ -1,15 +1,18 @@
 import sys
 
-print('*'*10,  "start" , '*'*10 )
+print("*" * 10, "start", "*" * 10)
 
-print("サイコロを降ってください (止めるときはqを押してください)")
-try:
-    dummy=input()
-except KeyboardInterrupt as e:
-    import pdb
-    print("文字を入れろよデコスケ野郎！")
-if dummy=='q':
-    print('ゲームを終わりにします')
-    sys.exit(1)
+while True:
+    print("サイコロを降ってください (止めるときはqを押してください)")
+    try:
+        dummy = input()
+    except KeyboardInterrupt as e:
+        import pdb
 
-print('end!')
+        print("文字を入れろよデコスケ野郎！")
+    if dummy == "q":
+        print("ゲームを終わりにします")
+        sys.exit(1)
+    print("入力された値は", dummy, "です。")
+print("end!")
+
