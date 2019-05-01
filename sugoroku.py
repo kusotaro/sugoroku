64ee1003d589008cf99bbfd7bda20746ea887e80
kusotaro <kusotaro@gmail.com>
2019-05-01 18:25:15 +0900

    サイコロの目をランダムに出すようにした

--- sugoroku.py
+++ sugoroku.py
@@ -1,18 +1,17 @@
 import sys
+import random
 
 print("*" * 10, "start", "*" * 10)
 
 while True:
-    print("サイコロを降ってください (止めるときはqを押してください)")
     try:
-        dummy = input()
-    except KeyboardInterrupt as e:
-        import pdb
-
-        print("文字を入れろよデコスケ野郎！")
+        dummy = input("サイコロを降ってください (止めるときはqを押してください)")
+    except (KeyboardInterrupt, EOFError) as e:
+        print("止められませんよ")
     if dummy == "q":
         print("ゲームを終わりにします。")
         sys.exit(1)
-    print("入力された値は", dummy, "です。")
+    dice_num = random.randint(1, 6)
+    print("サイコロの目は", dice_num, "です。")
 print("end!")
 
