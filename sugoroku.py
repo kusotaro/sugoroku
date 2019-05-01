f1407ce509b47b97f27528c2e37cafcc273b04db
kusotaro <kusotaro@gmail.com>
2019-05-02 00:07:40 +0900

    ゴールを追加した

--- sugoroku.py
+++ sugoroku.py
@@ -3,15 +3,20 @@ import random
 
 print("*" * 10, "start", "*" * 10)
 
+goal = 30
 while True:
     try:
         dummy = input("サイコロを降ってください (止めるときはqを押してください)")
-    except (KeyboardInterrupt, EOFError) as e:
+    except (KeyboardInterrupt, EOFError):
         print("止められませんよ")
     if dummy == "q":
         print("ゲームを終わりにします。")
         sys.exit(1)
     dice_num = random.randint(1, 6)
     print("サイコロの目は", dice_num, "です。")
-print("end!")
-
+    goal -= dice_num
+    if goal > 0:
+        print("ゴールまで", goal, "です。")
+    else:
+        print("ゴールしました")
+        sys.exit(0)
